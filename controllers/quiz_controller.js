9fab98046f13ad84c2661f113fd3b40e9bba6466
jarezal <jonan.lejarza@gmail.com>
2015-06-05 16:01:35 +0000

    BD

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,18 @@
+var models = require('../models/models.js');
+
 //GET /quizes/question
 
 exports.question = function(req,res) {
-        res.render('quizes/question', {pregunta: 'Capital de Italia'});
+        models.Quiz.findAll().success(function(quiz){
+        res.render('quizes/question', {pregunta: quiz[0].pregunta})
+        })
 };
 
 //GET /quizes/answer
 
 exports.answer = function(req,res) {
-    if (req.query.respuesta.toUpperCase() === 'ROMA')
+    models.Quiz.findAll().success(function(quiz){
+    if (req.query.respuesta.toUpperCase() === quiz[0].respuesta.toUpperCase())
     {
         res.render('quizes/answer', {respuesta: 'Correcto'});
     }
@@ -15,6 +20,7 @@ exports.answer = function(req,res) {
     {
         res.render('quizes/answer', {respuesta: 'Incorrecto'});
     }
+    })
 };
 
 exports.author = function(req,res) {
