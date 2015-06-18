f5187fef8762616e864b884d9ddcb1809f98f189
jarezal <jonan.lejarza@gmail.com>
2015-06-18 15:57:10 +0000

    módulo 9

--- controllers/statistics_controller.js
+++ controllers/statistics_controller.js
@@ -4,9 +4,9 @@ exports.statistics = function(req,res){
     
     var statistics ={countPreguntas: 0, countComentarios: 0, mediaComentarios: 0, pregComm: 0, pregNoComm:0};
     
-    models.sequelize.query('SELECT count(*) AS preguntas FROM "Quizzes"').then(function(countPreguntas){
+    models.sequelize.query('SELECT count(*) AS "preguntas" FROM "Quizzes"').then(function(countPreguntas){
         statistics.countPreguntas = countPreguntas[0].preguntas;
-        models.sequelize.query('SELECT count(*) AS comentarios FROM "Comments"').then(function(countComentarios){
+        models.sequelize.query('SELECT count(*) AS "comentarios" FROM "Comments"').then(function(countComentarios){
             statistics.countComentarios = countComentarios[0].comentarios;
             if(statistics.countPreguntas >0)
             {
